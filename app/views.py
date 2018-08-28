bda6e7869c351ed6bca91de6525ea7765c69a96b
melisamalala <melissamalala@gmail.com>
2018-08-28 16:30:43 +0300

    Added variable block

--- app/views.py
+++ app/views.py
@@ -8,4 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'Welcome to News API'
+    return render_template('index.html', message = message)
