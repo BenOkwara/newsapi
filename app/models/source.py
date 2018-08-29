09f6f6fe4a6f0971283ac2ab7fe94f8110d2dabb
melisamalala <melissamalala@gmail.com>
2018-08-29 09:53:52 +0300

    Added Docstrings to the Source Class

--- app/models/source.py
+++ app/models/source.py
@@ -1,25 +1,21 @@
-import unittest
-from models import source
-Source - source.Source
-
-class SourceTest(unittest.TestCase):
+class Source:
     '''
-        Test Class to test the behaviour of the News Source class
+        News Source class to define Source Objects
         '''
-    def setUp(self):
-        '''
-                Set up method that will run before every Test
-                '''
-        self.new_source = Source("abc-news",
-                                 'ABC News',
-                                 "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.",
-                                  "http://abcnews.go.com",
-                                 "general",
-                                 "en",
-                                 "us")
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_source, Source))
+    def __init__(self, id, name, description, url, category, country ):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
 
-    if __name__ == '__main__':
-        unittest.main()
+    '''
+    1. ID - The name of the news source according to its identification tag, e.g BBC-news, CNN, ABC
+    2. Name - The actual name of the news company
+    3. Description - A brief description of the new company
+    4. URL - The URL link or website to the news company 
+    5. Category - The category of news ,e.g entertainment, business, technology
+    6. Country - The country that the news originates from, e.g ABC News is from USA
+    '''
