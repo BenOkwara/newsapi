444a1ff24ea0e189834673531c603857fcf2568e
melisamalala <melissamalala@gmail.com>
2018-08-29 09:34:59 +0300

    Added the News API URL and SOURCE URL to allow users to search for source and articles

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,20 @@ from flask import Flask
 from .config import DevConfig
 
 # Initializing application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
+
+
+
+# We pass in instance_relative_config
+# which allow us to connect to the instance/folder
+# when the app instance is created.
+#
+# The app.config.from_pyfile('config.py')
+#  connects to the config.py file and all its
+# contents are appended to the app.config.
--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ class Config:
     General configuration parent class
     '''
     pass
-
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&country={}&category={}&apiKey={}'
+    SOURCE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
 
 class ProdConfig(Config):
